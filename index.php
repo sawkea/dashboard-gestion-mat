c72b732b715b95af2edbc7a4a551d30f5e6e38f0
sawkea <shoareau@orange.fr>
2020-07-27 12:02:22 +0200

    Initialisation du projet avec twig

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+$template = $twig->load('base.html.twig');
+echo $template->render();
+
