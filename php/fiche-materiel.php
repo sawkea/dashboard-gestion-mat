fcc7dbd54782d7ac6214bf1dea39ffd65d8bc7ad
sawkea <shoareau@orange.fr>
2020-07-27 14:40:16 +0200

    Updates fichiers twig - php - js de base

---
+++ php/fiche-materiel.php
@@ -0,0 +1,11 @@
+<?php 
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+$template = $twig->load('fiche-materiel.html.twig');
+echo $template->render();
