7f6bdf583e8f6d56c5a761bae7fab3eb035fe881
sawkea <shoareau@orange.fr>
2020-07-28 10:09:37 +0200

    Updates pages php à la bonne place

---
+++ ajout.php
@@ -0,0 +1,11 @@
+<?php 
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+$template = $twig->load('pages/ajout.html.twig');
+echo $template->render();
---
+++ fiche-materiel.php
@@ -0,0 +1,12 @@
+<?php 
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => false,
+]);
+
+
+
+$template = $twig->load('pages/fiche-materiel.html.twig');
+echo $template->render();
