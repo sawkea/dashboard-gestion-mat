87e4566e97a113e566af6ae1456c1c43130e9cf1
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-07-28 22:13:42 +0200

    login php - non fonctionnel

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
-<?php 
+<?php
+session_start(); 
 require_once 'vendor/autoload.php';
+require_once('db.php');
+$mdp = '';
+$pseudo = '';
+$msg = '';
+
+if(isset($_POST['envoyer'])&&  !empty($_POST['user']) && !empty($_POST['mdp'])){
+        
+    $sql="SELECT pseudo, mot_de_passe FROM login WHERE pseudo = :pseudo";
+    $sth = $dbh->prepare($sql);
+    $sth->bindParam(':pseudo', $_POST['pseudo'], PDO::PARAM_STR);  
+    $sth->execute();
+    $data = $sth->fetch();
+
+    $pseudo = $data['pseudo'];
+    $mdp = $data['mot_de_passe'];
+    
+    if($_POST['pseudo'] == $pseudo && $_POST['mdp'] == $mdp){
+        $_SESSION['valid'] = true;
+        $_SESSION['timeout'] = time();
+        $_SESSION['pseudo'] = $pseudo;
+        $_SESSION['mdp'] = $mdp;
+        header('Location: listing.php');
+    }else{
+        $msg = "Le nom d'utilisateur ou mot de passe est invalide";
+        
+    }
+}
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
@@ -7,7 +35,7 @@ $twig = new \Twig\Environment($loader, [
 ]);
 
 
-
 $template = $twig->load('pages/index.html.twig');
-echo $template->render();
+echo $template->render(['message', $msg]);
+
 
