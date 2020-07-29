81bbfa7ae5b907b1b128b4eaf7973c92595b624d
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-07-29 15:11:37 +0200

    affichage du tableau

--- listing.php
+++ listing.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 require_once 'vendor/autoload.php';
-require_once('db.php');
+
 if(empty($_SESSION['pseudo'])&& empty($_SESSION['mdp'])){
     header('Location: index.php');
 }
@@ -11,30 +11,19 @@ $twig = new \Twig\Environment($loader, [
     'cache' => false,
 ]);
 
-$sql = "SELECT reference, nom, categorie, date_achat FROM produit";
-$sth = $dbh->prepare($sql);  
-$sth->execute();
-
-$resultat = $sth->fetchAll(PDO::FETCH_ASSOC); 
-$intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
-foreach($resultat as $ligne){
-    echo '<tr>';
-        echo'<td>' . $ligne['id'] . '</td> ';
-        echo'<td>' . $ligne['reference'] . '</td> ';
-        echo'<td>' . $ligne['nom'] . '</td> ';
-        echo'<td>' . $ligne['categorie'] . '</td> ';
-        echo '<td>'.$intlDateFormater->format(strtotime($ligne['date_achat'])).'</td>';
-        echo '<td><a class="btn btn-outline-success marge-d pos-r" href="edit.php?edit=1&id='.$ligne['id'].'"><i class="fas fa-edit"></i></a></td>';
-        echo '<td><a class="btn btn-outline-danger btn_delete pos-r" href="delete.php?id='.$ligne['id'].'" ><i class="fas fa-trash-alt"></i></a></td>';
-    echo '</tr>';
+function liste_produit(){
+    require('db.php');
+    $sql = "SELECT id, reference, nom, categorie_id, date_achat FROM produit";
+    $sth = $dbh->prepare($sql);  
+    $sth->execute();
+    return $sth;
 }
+
+ 
+$intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
 
 
 $template = $twig->load('pages/listing.html.twig');
 echo $template->render(array(
-    'id' => '1',
-    'reference' => 'RH7221WO',
-    'nom' => 'ROWENTA X-PERT 160',
-    'categorie' => 1,
-    'date_achat' => 2020-07-05,
+    'resultat' => liste_produit(),
 ));
