a8c0a9842f813603b3e3b82c03beb3b6d5bc47fd
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-07-30 10:24:11 +0200

    ajout lecture caratctère spéciaux

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ define('HOST', 'localhost');
 
 
 try{
-    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD);
+    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 }catch (PDOException $e){
     print "Erreur !: " . $e->getMessage() . "<br/>";
