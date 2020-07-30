35ed2057909702d8dcec7eba9143bf5e9ba8bd0f
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-07-30 10:43:53 +0200

    update bdd changement de nom de table et champs

--- bdd/dgma.sql
+++ bdd/dgma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 29 juil. 2020 à 12:20
+-- Généré le :  jeu. 30 juil. 2020 à 07:33
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.4.0
 
@@ -74,27 +74,6 @@ INSERT INTO `categorie` (`id`, `nom`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `e-commerce`
---
-
-DROP TABLE IF EXISTS `e-commerce`;
-CREATE TABLE IF NOT EXISTS `e-commerce` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `site` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `e-commerce`
---
-
-INSERT INTO `e-commerce` (`id`, `site`) VALUES
-(1, 'www.amazon.fr/\r\n'),
-(2, 'www.cdiscount.com\r\n');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `login`
 --
 
@@ -133,21 +112,42 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `facture` varchar(255) DEFAULT NULL,
   `manuel_utilisation` varchar(255) DEFAULT NULL,
   `boutique_id` int(11) DEFAULT NULL,
-  `e-commerce_id` int(11) DEFAULT NULL,
+  `site_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `categorie_id` (`categorie_id`),
   KEY `boutique_id` (`boutique_id`),
-  KEY `e-commerce_id` (`e-commerce_id`)
+  KEY `e-commerce_id` (`site_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `fin_garantie`, `prix`, `conseils_entretien`, `facture`, `manuel_utilisation`, `boutique_id`, `e-commerce_id`) VALUES
+INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `fin_garantie`, `prix`, `conseils_entretien`, `facture`, `manuel_utilisation`, `boutique_id`, `site_id`) VALUES
 (1, 'ROWENTA X-PERT 160', 'RH7221WO', 1, '2020-07-05', '2021-07-05', 170.42, 'nettoyer le filtre après chaque utilisation', 'facture/row-ticket.jpg', 'manuel/row-guide.pdf', 2, NULL),
 (2, 'Refrigerateur americain', 'Lg GSJ470DIDV', 1, '2020-07-01', '2022-07-01', 1499, '-changer les filtres tous les 6 mois\r\n-nettoyer régulièrement', 'facture/frigoam-facture.jpg', NULL, 3, NULL),
 (3, 'TV LED Samsung', 'UE50TU7125', 3, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer\r\ndébrancher en cas d\'orage', 'facture/tv-facture.jpg', 'manuel/notice.pdf', 3, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `site`
+--
+
+DROP TABLE IF EXISTS `site`;
+CREATE TABLE IF NOT EXISTS `site` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `site`
+--
+
+INSERT INTO `site` (`id`, `url`) VALUES
+(1, 'www.cdiscount.com'),
+(2, 'www.amazon.fr');
 
 --
 -- Contraintes pour les tables déchargées
@@ -158,7 +158,8 @@ INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `
 --
 ALTER TABLE `produit`
   ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`boutique_id`) REFERENCES `boutique` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`boutique_id`) REFERENCES `boutique` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produit_ibfk_3` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
