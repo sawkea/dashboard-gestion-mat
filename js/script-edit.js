9ac065507be3249e0e986f8d31e414a625047a47
sawkea <shoareau@orange.fr>
2020-08-18 14:31:57 +0200

    Updates script pour griser les champs du form selon le choix du lieux de vente

---
+++ js/script-edit.js
@@ -0,0 +1,30 @@
+// sélectionner les input de selection les mettre non griser selon la selection
+
+// DECLARATION DES VARIABLES POUR RECUPERER LES ID DU SELECTLIEU
+
+var selectElem = document.getElementById('selectlieu');
+var boxurl = document.getElementById('boxurl');
+var inputAddress = document.getElementById('inputAddress');
+var inputVille = document.getElementById('inputVille');
+var inputCP = document.getElementById('inputCP');
+
+
+console.log(boxurl);
+
+// FONCTION POUR GRISER LES CHAMPS SELON LE CHOIX 
+// Quand une nouvelle <option> est selectionnée
+selectElem.addEventListener('change', function() {
+  // si cetteoption est égale à la valeur boutique
+  if (this.value == "boutique") {
+      // alors on ajoute l'attribut disabled
+    boxurl.setAttribute("disabled", "");
+  }
+  else {
+    // sinon on enlève l'attribut disabled
+    boxurl.removeAttribute("disabled");
+    inputAddress.setAttribute("disabled", "");
+    inputVille.setAttribute("disabled", "");
+    inputCP.setAttribute("disabled", "");
+    }
+});
+
