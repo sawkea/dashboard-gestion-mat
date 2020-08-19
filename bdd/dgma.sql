8e1cb7b4f1e24fc94c6971e9a727125945069473
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-08-19 10:16:37 +0200

    update bdd
    update mdp, nettoyage des tables

--- bdd/dgma.sql
+++ bdd/dgma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 30 juil. 2020 à 14:11
+-- Généré le :  mer. 19 août 2020 à 08:13
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.4.0
 
@@ -21,32 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `dgma`
 --
-
--- --------------------------------------------------------
-
---
--- Structure de la table `boutique`
---
-
-DROP TABLE IF EXISTS `boutique`;
-CREATE TABLE IF NOT EXISTS `boutique` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(45) NOT NULL,
-  `adresse` varchar(255) DEFAULT NULL,
-  `ville` varchar(55) DEFAULT NULL,
-  `cp` varchar(6) DEFAULT NULL,
-  `pays` varchar(45) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `boutique`
---
-
-INSERT INTO `boutique` (`id`, `nom`, `adresse`, `ville`, `cp`, `pays`, `url`) VALUES
-(2, 'BOULANGER', 'chateaufarine (centre commercial)', 'Besançon', '25000', 'France', NULL),
-(3, 'DARTY', 'chateaufarine (centre commercial)', 'Besançon', '25000', 'France', NULL);
 
 -- --------------------------------------------------------
 
@@ -83,15 +57,16 @@ CREATE TABLE IF NOT EXISTS `login` (
   `pseudo` varchar(55) NOT NULL,
   `mot_de_passe` varchar(55) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `login`
 --
 
 INSERT INTO `login` (`id`, `pseudo`, `mot_de_passe`) VALUES
-(1, 'admin', '*F757EE7C8726B3275D528FB21814A9A3743446FE'),
-(3, 'Lisa', 'Vaneil');
+(5, 'sonia', 'sonia'),
+(4, 'lisa', 'lisa'),
+(6, 'admin', 'pass');
 
 -- --------------------------------------------------------
 
@@ -111,46 +86,25 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `conseils_entretien` text NOT NULL,
   `facture` varchar(255) DEFAULT NULL,
   `manuel_utilisation` varchar(255) DEFAULT NULL,
-  `boutique` int(11) DEFAULT NULL,
   `adresse` varchar(255) DEFAULT NULL,
   `ville` varchar(55) DEFAULT NULL,
   `cp` varchar(6) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `categorie_id` (`categorie_id`),
-  KEY `boutique_id` (`boutique`),
-  KEY `boutique` (`boutique`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  KEY `categorie_id` (`categorie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `fin_garantie`, `prix`, `conseils_entretien`, `facture`, `manuel_utilisation`, `boutique`, `adresse`, `ville`, `cp`, `url`) VALUES
-(1, 'ROWENTA X-PERT 160', 'RH7221WO', 1, '2020-07-05', '2021-07-05', 170.42, 'nettoyer le filtre après chaque utilisation', 'facture/row-ticket.jpg', 'manuel/row-guide.pdf', 2, '', '', '', ''),
-(2, 'Refrigerateur americain', 'Lg GSJ470DIDV', 1, '2020-07-01', '2022-07-01', 1499, '-changer les filtres tous les 6 mois\r\n-nettoyer régulièrement', 'facture/frigoam-facture.jpg', NULL, 3, '', '', '', ''),
-(3, 'TV LED Samsung', 'UE50TU7125', 3, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer\r\ndébrancher en cas d\'orage', 'facture/tv-facture.jpg', 'manuel/notice.pdf', 3, '', '', '', '');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `site`
---
-
-DROP TABLE IF EXISTS `site`;
-CREATE TABLE IF NOT EXISTS `site` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `site`
---
-
-INSERT INTO `site` (`id`, `url`) VALUES
-(1, 'www.cdiscount.com'),
-(2, 'www.amazon.fr');
+INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `fin_garantie`, `prix`, `conseils_entretien`, `facture`, `manuel_utilisation`, `adresse`, `ville`, `cp`, `url`) VALUES
+(1, 'ROWENTA X-PERT 160', 'RH7221WO', 1, '2020-07-05', '2021-07-05', 170.42, 'nettoyer le filtre après chaque utilisation', 'facture/row-ticket.jpg', 'manuel/row-guide.pdf', '', '', '', ''),
+(3, 'TV LED Samsung', 'UE50TU7125', 3, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer\r\ndébrancher en cas d\'orage', 'facture/tv-facture.jpg', 'manuel/notice.pdf', '', '', '', ''),
+(6, 'Micro-onde Whirpool', 'LM826 BV', 1, '2020-08-17', '2020-08-18', 11, '-ne pas couvrir l\'aeration', '', '', '44 rue marmotte', 'Besançon', '25000', ''),
+(11, 'TV LED Sony', 'UE50TU7125', 3, '2020-06-12', '2021-06-16', 1266.99, 'dépoussiererdébrancher en cas d\'orage', '', '', '44 rue marmotte', 'Besançon', '25000', ''),
+(12, 'Tablette Sony', '120UE4659DJF0', 4, '2020-08-17', '2021-08-17', 350, 'attendre que la batterie soit vide avant de recharger', '', '', 'Boulanger chateaufarine', 'Besançon', '25000', ''),
+(13, 'TV LED Samsung', 'UE50TU7125', 2, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer débrancher en cas d\'orage', '', '', '', '', '', '');
 
 --
 -- Contraintes pour les tables déchargées
