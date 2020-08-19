709b1ed9783a13e9753daf0c57c9609691f44244
LisaDevoghelaeregit config --global user.name LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-08-19 15:44:30 +0200

    nom de champs

--- bdd/dgma.sql
+++ bdd/dgma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 19 août 2020 à 08:13
+-- Généré le :  mer. 19 août 2020 à 13:43
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.4.0
 
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `fin_garantie` date NOT NULL,
   `prix` float NOT NULL,
   `conseils_entretien` text NOT NULL,
-  `facture` varchar(255) DEFAULT NULL,
+  `ticket` varchar(255) DEFAULT NULL,
   `manuel_utilisation` varchar(255) DEFAULT NULL,
   `adresse` varchar(255) DEFAULT NULL,
   `ville` varchar(55) DEFAULT NULL,
@@ -92,19 +92,20 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `categorie_id` (`categorie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `produit`
 --
 
-INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `fin_garantie`, `prix`, `conseils_entretien`, `facture`, `manuel_utilisation`, `adresse`, `ville`, `cp`, `url`) VALUES
-(1, 'ROWENTA X-PERT 160', 'RH7221WO', 1, '2020-07-05', '2021-07-05', 170.42, 'nettoyer le filtre après chaque utilisation', 'facture/row-ticket.jpg', 'manuel/row-guide.pdf', '', '', '', ''),
+INSERT INTO `produit` (`id`, `nom`, `reference`, `categorie_id`, `date_achat`, `fin_garantie`, `prix`, `conseils_entretien`, `ticket`, `manuel_utilisation`, `adresse`, `ville`, `cp`, `url`) VALUES
+(1, 'ROWENTA X-PERT 160', 'RH7221WO', 1, '2020-07-05', '2021-07-05', 170.42, 'nettoyer le filtre après chaque utilisation', 'produit/ticket/RH7221WO.pdf', '', 'Boulanger chateaufarine', 'Besançon', '', ''),
 (3, 'TV LED Samsung', 'UE50TU7125', 3, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer\r\ndébrancher en cas d\'orage', 'facture/tv-facture.jpg', 'manuel/notice.pdf', '', '', '', ''),
 (6, 'Micro-onde Whirpool', 'LM826 BV', 1, '2020-08-17', '2020-08-18', 11, '-ne pas couvrir l\'aeration', '', '', '44 rue marmotte', 'Besançon', '25000', ''),
 (11, 'TV LED Sony', 'UE50TU7125', 3, '2020-06-12', '2021-06-16', 1266.99, 'dépoussiererdébrancher en cas d\'orage', '', '', '44 rue marmotte', 'Besançon', '25000', ''),
 (12, 'Tablette Sony', '120UE4659DJF0', 4, '2020-08-17', '2021-08-17', 350, 'attendre que la batterie soit vide avant de recharger', '', '', 'Boulanger chateaufarine', 'Besançon', '25000', ''),
-(13, 'TV LED Samsung', 'UE50TU7125', 2, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer débrancher en cas d\'orage', '', '', '', '', '', '');
+(13, 'TV LED Samsung', 'UE50TU7125', 2, '2020-06-16', '2021-06-16', 449.99, 'dépoussierer débrancher en cas d\'orage', '', '', '', '', '', ''),
+(23, 'Micro-onde Whirpool', 'LM826 BV', 1, '2020-08-19', '2020-08-19', 2, '-ne pas couvrir l\'aeration', 'produit/ticket/LM826 BV.pdf', '', '44 rue marmotte', 'Ankh Morpok', '', '');
 
 --
 -- Contraintes pour les tables déchargées
