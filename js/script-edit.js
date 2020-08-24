a8f12439ab34e56be15bdf7ef674fd8477a7a3bc
sawkea <shoareau@orange.fr>
2020-08-24 15:22:44 +0200

    Updates script edit

--- js/script-edit.js
+++ js/script-edit.js
@@ -28,13 +28,13 @@ selectElem.addEventListener('change', function() {
     }
 });
 
-// label formulaire labelFile
-function labelFile(file){
-  const txtLabelfile = document.getElementById('ticket');
-  txtLabelfile.innerText = file[0].name;
-}
-function labelManuel(file){
-  const txtLabelfile = document.getElementById('manuel');
-  txtLabelfile.innerText = file[0].name;
-}
+// // label formulaire labelFile
+// function labelFile(file){
+//   const txtLabelfile = document.getElementById('ticket');
+//   txtLabelfile.innerText = file[0].name;
+// }
+// function labelManuel(file){
+//   const txtLabelfile = document.getElementById('manuel');
+//   txtLabelfile.innerText = file[0].name;
+// }
 
